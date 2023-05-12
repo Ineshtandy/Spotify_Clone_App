d9223cf87b9f370afcd77b1dabd12b6bcce094f4
Inesh Tandon <i08polymath@gmail.com>
2023-05-12 22:55:46 +0530

    app js update -recieving hash value -extracting token from it inside useEffect -storing token in a useState -clearing out the url

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./components/Login";
+import { getTokenFromResponse } from "./spotify";
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromResponse();
+    //this removes the access token from the url for security reasons but we have it stored in hash
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <div className="App">{token ? <h1>I am Logged in</h1> : <Login />}</div>
   );
 }
 
