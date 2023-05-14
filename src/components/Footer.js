698d4ca7efbaf392479cfd3951125c8574af2a19
Inesh Tandon <i08polymath@gmail.com>
2023-05-14 22:42:10 +0530

    Footer styling -divided footer in 3 sections -added styling to all 3 -not functional yet

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,48 @@
 import React from "react";
 import "../Styles/Footer.css";
+import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+import ShuffleIcon from "@mui/icons-material/Shuffle";
+import RepeatIcon from "@mui/icons-material/Repeat";
+import VolumeDownIcon from "@mui/icons-material/VolumeDown";
+import PauseCircleOutlineIcon from "@mui/icons-material/PauseCircleOutline";
+import PlaylistPlayIcon from "@mui/icons-material/PlaylistPlay";
+import Slider from "@mui/material/Slider";
+import Grid from "@mui/material/Grid";
 
 function Footer() {
   return (
     <div className="footer">
-      <h1>i am footer</h1>
+      <div className="footer__left">
+        <img className="footer__albumLogo" src="" alt="your mom" />
+        <div className="footer__songInfo">
+          <h4>yeah</h4>
+          <p>eminem</p>
+        </div>
+      </div>
+
+      <div className="footer__center">
+        <ShuffleIcon className="footer__green" />
+        <SkipPreviousIcon className="footer__icon" />
+        <PlayCircleOutlineIcon fontSize="large" className="footer__icon" />
+        <SkipNextIcon className="footer__icon" />
+        <RepeatIcon className="footer__green" />
+      </div>
+
+      <div className="footer__right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider aria-labelledby="continuous-slider" />
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
